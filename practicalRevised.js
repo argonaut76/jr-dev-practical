36848e5687794c054911c9389dd82e855d1c9cd5
Nina Mazzarelli <ninamazz70@gmail.com>
2020-08-24 20:06:13 -0400

    upload

--- practicalRevised.js
+++ practicalRevised.js
@@ -1,21 +1,32 @@
-// 1. Write a function that determines if a string starts with an upper-case letter.
+// 1. Write a function that determines if a string starts with an upper-case letter. 
 
 function returnUpperCase(inputStr) {
-  return inputStr.split("")[0].contains(/A-Z/)
+  return inputStr.split("")[0].contains(RegExp("/^[A-Z]/"))
+}
+
+////Better way without regex 
+
+function returnUpperCase(inputStr) {
+  const firstChar = inputStr.split("")[0]
+  return firstChar === firstChar.toUpperCase()
 }
 
 // 2. Write a function in a language of your choosing that removes punctuation as fast as possible from an array of strings.
 
+var punct = /[.!?;:`,'"]/g;
+
 function removePunct(inputArray) {
-  inputArray.map(element => {
-    for (x=0; x < elements.length; x++ ) {
-      x.contains(/puctuation/) ? element.splice(0,x) : null
-    return element
-  })
+  inputArray.map((element) => {
+    return element.replace(punct, "");
+  });
 }
 
 
 // 3. Write a single line of JavaScript that returns the total impact score of all articles in the following "documents" array
+
+documents.map(element => { element.citations.reduce( (sum, val) => sum + get_impact_score(val) ) })
+
+
 function get_impact_score(citations,year) {
 	let yhat = 0.015 * year + 27;	
 	return citations - yhat;
@@ -59,15 +70,36 @@ const documents = [
   }
 ];
 
-documents.map(element => {return const sum.reduce(x => x.get_impact_score(element.citations, element.year))})
 
 // 4. Using vanilla HTML and CSS, code a table that will present the data in the "documents" array from question 4.
 
+
+// <!DOCTYPE html>
+// <html lang="en"></html>
+
+{documents.map(element => {  
+  <table>
+      <tr>
+          <th >{element.title}</th>   
+      </tr>
+      <tr>
+          <td>{element.year}</td>
+          <td>{element.citations}</td>
+      </tr>
+  </table>
+})}
 
 
 
 // 5. Imagine a reddit-style website where users can post comments, reply to other user's comments, reply to replies, and upvote comments.  
 //    How would you write a recursive function that sums the upvotes for a top-level comment and all nested replies? 
 
+//premise: recursive function sums until condition (# nested comments exceded) is satisfied
 
+function someRecurse(comments) { 
+  var likeSum = 0;
+  while (comments) {
+     likeSum ++;
+  }
+}
 
